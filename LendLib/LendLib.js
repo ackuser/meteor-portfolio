442fb44446d64747f85f211bf250e1b2240bc607
ackuser <mail@ackarim.com>
2015-09-16 17:33:55 +0100

    Adding some functionalities for todo list

--- LendLib/LendLib.js
+++ LendLib/LendLib.js
@@ -2,6 +2,8 @@ lists = new Mongo.Collection("lists");
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
+  // We are declaring the 'adding_category' flag
+  Session.set('adding_category', false);
 
   Template.hello.helpers({
     counter: function () {
@@ -9,9 +11,34 @@ if (Meteor.isClient) {
     }
   });
   Template.categories.helpers({
-  lists : function(){
-    return lists.find({}, { sort: { Category : 1 }});
-  }
+    lists : function(){
+      return lists.find({}, { sort: { Category : 1 }});
+    },
+    new_cat: function(){
+      //returns true if adding_category has been assigned
+      //a value of true
+      return Session.equals('adding_category',true);
+    }
+  });
+  Template.categories.events({
+    'click #btnNewCat': function (e, t) {
+      Session.set('adding_category', true);
+      Tracker.flush();
+      focusText(t.find("#add-category"));
+    },
+    'keyup #add-category': function (e,t){
+      if (e.which === 13)
+      {
+        var catVal = String(e.target.value || "");
+        if (catVal){
+          lists.insert({Category:catVal});
+          Session.set('adding_category', false);
+        }
+      }
+    },
+    'focusout #add-category': function(e,t){
+      Session.set('adding_category',false);
+    }
   });
   Template.hello.events({
     'click button': function () {
